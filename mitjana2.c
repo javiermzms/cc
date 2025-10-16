a83c8c8ff582963375920b522ae1f764abd98c06
javiermzms <informaticac163@gmail.com>
2025-10-16 10:36:49 +0200

    Create mitjana2.c

---
+++ mitjana2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int a, b, mitjana;
+    printf("1r num?: ");
+    scanf ("%d", &a);
+    printf("2n num?: ");
+    scanf ("%d", &b);
+    printf("La mitjana es: %d\n", (a+b)/2);
+    return;
+
+}
