e097852367ca8735bf39797deb3b59922793cf09
javiermzms <informaticac163@gmail.com>
2025-11-05 13:39:35 +0100

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -5,12 +5,13 @@ int fibonacci(int n)
     int aux;
     if(n==0) aux=1;
         else if (n==1) aux=1;
-                  else aux=fibonacc(n-1)+fibonacci(n-2);
+                  else aux=fibonacci(n-1)+fibonacci(n-2);
         return aux;
 }
 void main(){
     int n;
     printf("Fibonacci de: ");
-    scanf ("%d\n", fibonacci(n));
+    scanf("%d" ,&n);
+    printf ("%d\n", fibonacci(n));
     return;
 }
